35d1b53ace448bc5c0346f66114bdc47743f97a2
目棃 <BT-Muli@outlook.com>
2024-12-02 17:24:39 +0800

    🔧 引入一些插件，调整打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import vue from "@vitejs/plugin-vue";
+import IconsResolver from "unplugin-icons/resolver";
+import Icons from "unplugin-icons/vite";
+import Components from "unplugin-vue-components/vite";
 import { defineConfig } from "vite";
-
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Icons({ autoInstall: true, compiler: "vue3" }),
+    Components({
+      resolvers: [IconsResolver({ prefix: "icon" })],
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -17,16 +25,19 @@ export default defineConfig(async () => ({
     port: 1420,
     strictPort: true,
     host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+    hmr: host ? { protocol: "ws", host, port: 1421 } : undefined,
+    watch: { ignored: ["**/src-tauri/**"] },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            const arr = id.split("node_modules/");
+            return arr[2].split("/")[0];
+          }
+        },
+      },
     },
   },
 }));
