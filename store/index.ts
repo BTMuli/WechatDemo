9eb3b4238ef7d1f9335874528d7b12a5674c8fd1
目棃 <BT-Muli@outlook.com>
2024-12-03 14:04:27 +0800

    🔥 移除无用文件

--- store/index.ts
+++
@@ -1,8 +0,0 @@
-import { createPinia } from "pinia";
-import { createPersistedState } from "pinia-plugin-persistedstate";
-
-const store = createPinia();
-
-store.use(createPersistedState());
-
-export default store;
